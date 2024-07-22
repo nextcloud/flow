e56973894004026ff3237f22206521d0a98fb04a
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-07-22 14:49:06 +0300

    finished refactoring webhooks
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- ex_app/lib/main.py
+++ ex_app/lib/main.py
@@ -1,7 +1,6 @@
 """Windmill as an ExApp"""
 
 import asyncio
-import base64
 import contextlib
 import json
 import os
@@ -17,7 +16,7 @@ import httpx
 from fastapi import BackgroundTasks, Depends, FastAPI, Request, responses, status
 from nc_py_api import NextcloudApp
 from nc_py_api.ex_app import LogLvl, nc_app, persistent_storage, run_app
-from nc_py_api.ex_app.integration_fastapi import fetch_models_task
+from nc_py_api.ex_app.integration_fastapi import AppAPIAuthMiddleware, fetch_models_task
 from starlette.responses import FileResponse, Response
 
 # os.environ["NEXTCLOUD_URL"] = "http://nextcloud.local/index.php"
@@ -64,10 +63,22 @@ async def create_user(user_name: str) -> str:
 
 
 async def login_user(user_name: str, password: str) -> str:
+    print("login_user:DEBUG:", user_name, flush=True)
     async with httpx.AsyncClient() as client:
         r = await client.post(
             url="http://127.0.0.1:8000/api/auth/login",
             json={"email": f"{user_name}@windmill.dev", "password": password},
+        )
+        if r.status_code >= 400:
+            raise RuntimeError(f"login_user: {r.text}")
+        return r.text
+
+
+def login_user_sync(user_name: str, password: str) -> str:
+    with httpx.Client() as client:
+        r = client.post(
+            url="http://127.0.0.1:8000/api/auth/login",
+            json={"email": user_name, "password": password},
         )
         if r.status_code >= 400:
             raise RuntimeError(f"login_user: {r.text}")
@@ -80,6 +91,22 @@ async def check_token(token: str) -> bool:
         return bool(r.status_code < 400)
 
 
+def check_token_sync(token: str) -> bool:
+    with httpx.Client() as client:
+        r = client.get("http://127.0.0.1:8000/api/users/whoami", cookies={"token": token})
+        return bool(r.status_code < 400)
+
+
+def get_valid_user_token_sync(user_name: str) -> str:
+    token = USERS_STORAGE[user_name]["token"]
+    if check_token_sync(token):
+        return token
+    user_password = USERS_STORAGE[user_name]["password"]
+    token = login_user_sync(user_name, user_password)
+    add_user_to_storage(user_name, user_password, token)
+    return token
+
+
 async def provision_user(request: Request) -> None:
     if "token" in request.cookies:
         print(f"DEBUG: TOKEN IS PRESENT: {request.cookies['token']}", flush=True)
@@ -89,9 +116,8 @@ async def provision_user(request: Request) -> None:
 
     user_name = get_windmill_username_from_request(request)
     if user_name in USERS_STORAGE:
-        zzz = USERS_STORAGE[user_name]["token"]
-        aaa = await check_token(zzz)
-        if not USERS_STORAGE[user_name]["token"] or aaa is False:
+        windmill_token_valid = await check_token(USERS_STORAGE[user_name]["token"])
+        if not USERS_STORAGE[user_name]["token"] or windmill_token_valid is False:
             user_password = USERS_STORAGE[user_name]["password"]
             add_user_to_storage(user_name, user_password, await login_user(user_name, user_password))
     else:
@@ -107,7 +133,7 @@ async def lifespan(_app: FastAPI):
 
 
 APP = FastAPI(lifespan=lifespan)
-# APP.add_middleware(AppAPIAuthMiddleware)  # set global AppAPI authentication middleware
+APP.add_middleware(AppAPIAuthMiddleware)  # set global AppAPI authentication middleware
 
 
 def get_windmill_username_from_request(request: Request) -> str:
@@ -152,7 +178,7 @@ def enabled_callback(enabled: bool, nc: typing.Annotated[NextcloudApp, Depends(n
 
 @APP.api_route("/api/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE"])
 async def proxy_backend_requests(request: Request, path: str):
-    # print(f"proxy_BACKEND_requests: {path} - {request.method}\nCookies: {request.cookies}", flush=True)
+    print(f"proxy_BACKEND_requests: {path} - {request.method}\nCookies: {request.cookies}", flush=True)
     await provision_user(request)
     async with httpx.AsyncClient() as client:
         url = f"http://127.0.0.1:8000/api/{path}"
@@ -173,9 +199,9 @@ async def proxy_backend_requests(request: Request, path: str):
                 cookies=request.cookies,
                 content=await request.body(),
             )
-        # print(
-        #     f"proxy_BACKEND_requests: method={request.method}, path={path}, status={response.status_code}", flush=True
-        # )
+        print(
+            f"proxy_BACKEND_requests: method={request.method}, path={path}, status={response.status_code}", flush=True
+        )
         response_header = dict(response.headers)
         response_header.pop("transfer-encoding", None)
         return Response(content=response.content, status_code=response.status_code, headers=response_header)
@@ -190,10 +216,10 @@ async def proxy_frontend_requests(request: Request, path: str):
     if path.startswith("ex_app"):
         file_server_path = Path("../../" + path)
     elif not path or path == "user/login":
-        # file_server_path = Path("../../windmill_tmp/frontend/build/200.html")
+        # file_server_path = Path("../../windmill_tmp/frontend/build/200.html")  # JS DEBUG MODE
         file_server_path = Path("/iframe/200.html")
     else:
-        # file_server_path = Path("../../windmill_tmp/frontend/build/").joinpath(path)
+        # file_server_path = Path("../../windmill_tmp/frontend/build/").joinpath(path)  # JS DEBUG MODE
         file_server_path = Path("/iframe/").joinpath(path)
     if not file_server_path.exists():
         return Response(status_code=status.HTTP_404_NOT_FOUND)
@@ -255,39 +281,53 @@ def webhooks_syncing():
 
     while True:
         nc = NextcloudApp()
-        if nc.enabled_state:
-            print("Running workflow sync")
-            token = USERS_STORAGE[DEFAULT_USER_NAME]["token"]
-            flow_paths = get_flow_paths(workspace, token)
-            print(flow_paths)
-            expected_listeners = get_expected_listeners(workspace, token, flow_paths)
-            print(json.dumps(expected_listeners, indent=4), flush=True)
-            registered_listeners = get_registered_listeners()
-            # for expected_listener in expected_listeners:
-            #     registered_listeners_for_uri = get_registered_listeners_for_uri(expected_listener['webhook'], registered_listeners)
-            #     for event in expected_listener['events']:
-            #         listener = next(filter(lambda listener: listener['event'] == event, registered_listeners_for_uri), None)
-            #         if listener is not None:
-            #             json.dumps(listener)
-            #             if json.dumps(listener['eventFilter']) != json.dumps(expected_listener['filters']):
-            #                 update_listener(listener, expected_listener['filters'], token)
-            #         else:
-            #             register_listener(event, expected_listener['filters'], expected_listener['webhook'], token)
-            # registered_listeners = get_registered_listeners()
-            # for registered_listener in registered_listeners:
-            #     if registered_listener['appId'] == os.environ["APP_ID"]:
-            #         if next(filter(lambda expected_listener: registered_listener['uri'] == expected_listener['webhook'] and registered_listener['event'] in expected_listener['events'], expected_listeners), None) is None:
-            #             await delete_listener(registered_listener)
-        sleep(5 * 60)
+        if not nc.enabled_state:
+            sleep(5 * 60)
+            continue
+        print("Running workflow sync")
+        token = get_valid_user_token_sync(DEFAULT_USER_NAME)
+        flow_paths = get_flow_paths(workspace, token)
+        print(flow_paths)
+        expected_listeners = get_expected_listeners(workspace, token, flow_paths)
+        print(json.dumps(expected_listeners, indent=4), flush=True)
+        registered_listeners = get_registered_listeners()
+        for expected_listener in expected_listeners:
+            registered_listeners_for_uri = get_registered_listeners_for_uri(
+                expected_listener["webhook"], registered_listeners
+            )
+            for event in expected_listener["events"]:
+                listener = next(filter(lambda listener: listener["event"] == event, registered_listeners_for_uri), None)
+                if listener is not None:
+                    json.dumps(listener)
+                    if json.dumps(listener["eventFilter"]) != json.dumps(expected_listener["filters"]):
+                        update_listener(listener, expected_listener["filters"], token)
+                else:
+                    register_listener(event, expected_listener["filters"], expected_listener["webhook"], token)
+        registered_listeners = get_registered_listeners()
+        for registered_listener in registered_listeners:
+            if registered_listener["appId"] == nc.app_cfg.app_name:  # noqa
+                if (
+                    next(
+                        filter(
+                            lambda expected_listener: registered_listener["uri"] == expected_listener["webhook"]
+                            and registered_listener["event"] in expected_listener["events"],
+                            expected_listeners,
+                        ),
+                        None,
+                    )
+                    is None
+                ):
+                    delete_listener(registered_listener)
+        sleep(30)
 
 
 def get_flow_paths(workspace: str, token: str) -> list[str]:
     method = "GET"
-    path = f"/w/{workspace}/flows/list"
+    path = f"w/{workspace}/flows/list"
     print(f"sync_API_request: {path} - {method}", flush=True)
     flow_paths = []
     with httpx.Client() as client:
-        url = f"http://127.0.0.1:8000/api{path}"
+        url = f"http://127.0.0.1:8000/api/{path}"
         headers = {"Authorization": f"Bearer {token}"}
         # print(f"proxy_BACKEND_requests: method={request.method}, path={path}, status={response.status_code}")
         response = client.request(
@@ -314,7 +354,7 @@ def get_expected_listeners(workspace: str, token: str, flow_paths: list[str]) ->
     for flow_path in flow_paths:
         with httpx.Client() as client:
             method = "GET"
-            path = f"/w/{workspace}/flows/get/{flow_path}"
+            path = f"w/{workspace}/flows/get/{flow_path}"
             url = f"http://127.0.0.1:8000/api/{path}"
             headers = {"Authorization": f"Bearer {token}"}
             print(f"sync_API_request: {path} - {method}", flush=True)
@@ -360,161 +400,51 @@ def get_registered_listeners_for_uri(webhook: str, registered_listeners: list) -
     return [listener for listener in registered_listeners if listener["uri"] == webhook]
 
 
-# async def register_listener(event, filter, webhook, token):
-#     async with httpx.AsyncClient() as client:
-#         method = 'POST'
-#         path = '/ocs/v2.php/apps/webhook_listeners/api/v1/webhooks'
-#         url = os.environ["NEXTCLOUD_URL"]+path
-#         userid = os.environ["NEXTCLOUD_USERID"]
-#         secret = os.environ["NEXTCLOUD_SECRET"]
-#
-#         headers = {
-#
-#             'AUTHORIZATION-APP-API': base64.b64encode(bytes(f"{userid}:{secret}", 'utf-8')).decode('utf-8'),
-#             'OCS-APIREQUEST': 'true',
-#             'EX-APP-VERSION': 'v1.0.0',
-#             'EX-APP-ID': os.environ['APP_ID'],
-#             'AA-VERSION': 'v2.3.0'
-#         }
-#         auth_data = {
-#             'Authorization': f"Bearer {token}"
-#         }
-#         print(f"sync_API_nextcloud_request: {url} - {method}", flush=True)
-#         print(json.dumps(filter, indent=4), flush=True)
-#         response = await client.request(
-#             method=method,
-#             url=url,
-#             json={'httpMethod': 'POST', 'uri': webhook, 'event': event, 'eventFilter': filter, 'authMethod': 'header', 'authData': auth_data},
-#             headers=headers,
-#             params={'format': 'json'}
-#         )
-#         print(
-#             f"sync_API_nextcloud_request: method={method}, path={path}, status={response.status_code}",
-#             flush=True
-#         )
-#         try:
-#             response_data = json.loads(response.content)
-#         except json.JSONDecodeError as e:
-#             print(f"Error parsing JSON: {e}")
-#             return
-#         print(json.dumps(response_data, indent=4), flush=True)
-#         return response_data['ocs']['data']
+async def register_listener(event, event_filter, webhook, token: str) -> dict:
+    auth_data = {
+        "Authorization": f"Bearer {token}",
+    }
+    nc = NextcloudApp()
+    print(f"register_listener: {webhook} - {event}", flush=True)
+    print(json.dumps(event_filter, indent=4), flush=True)
+    r = nc.webhooks.register(
+        "POST", webhook, event, event_filter=event_filter, auth_method="header", auth_data=auth_data
+    )
+    print(json.dumps(r, indent=4), flush=True)
+    return r
 
 
-# async def update_listener(registered_listener, filter, token):
-#     async with httpx.AsyncClient() as client:
-#         method = 'POST'
-#         path = f"/ocs/v2.php/apps/webhook_listeners/api/v1/webhooks/{registered_listener['id']}"
-#         url = os.environ["NEXTCLOUD_URL"] + path
-#         userid = os.environ["NEXTCLOUD_USERID"]
-#         secret = os.environ["NEXTCLOUD_SECRET"]
-#
-#         headers = {
-#             'AUTHORIZATION-APP-API': base64.b64encode(bytes(f"{userid}:{secret}", 'utf-8')).decode('utf-8'),
-#             'OCS-APIREQUEST': 'true',
-#             'EX-APP-VERSION': 'v1.0.0',
-#             'EX-APP-ID': os.environ['APP_ID'],
-#             'AA-VERSION': 'v2.3.0'
-#         }
-#         auth_data = {
-#             'Authorization': f"Bearer {token}"
-#         }
-#         print(f"sync_API_nextcloud_request: {url} - {method}", flush=True)
-#         print(json.dumps(filter, indent=4), flush=True)
-#         response = await client.request(
-#             method=method,
-#             url=url,
-#             json={'httpMethod': 'POST', 'uri': registered_listener['uri'], 'event': registered_listener['event'], 'eventFilter': filter, 'authMethod': 'header',
-#                   'authData': auth_data},
-#             headers=headers,
-#             params={'format': 'json'}
-#         )
-#         print(
-#             f"sync_API_nextcloud_request: method={method}, path={path}, status={response.status_code}",
-#             flush=True
-#         )
-#         try:
-#             response_data = json.loads(response.content)
-#         except json.JSONDecodeError as e:
-#             print(f"Error parsing JSON: {e}")
-#             return
-#         print(json.dumps(response_data, indent=4), flush=True)
-#         return response_data['ocs']['data']
+def update_listener(registered_listener: dict, event_filter, token: str) -> dict:
+    auth_data = {
+        "Authorization": f"Bearer {token}",
+    }
+    nc = NextcloudApp()
+    print(f"update_listener: {registered_listener['uri']} - {registered_listener['event']}", flush=True)
+    print(json.dumps(event_filter, indent=4), flush=True)
+    r = nc.webhooks.update(
+        registered_listener["id"],
+        "POST",
+        registered_listener["uri"],
+        registered_listener["event"],
+        event_filter=event_filter,
+        auth_method="header",
+        auth_data=auth_data,
+    )
+    print(json.dumps(r, indent=4), flush=True)
+    return r
 
 
 def get_registered_listeners():
     nc = NextcloudApp()
-    try:
-        aaa = nc.ocs("GET", "/ocs/v1.php/apps/webhook_listeners/api/v1/webhooks")
-    except Exception as e:
-        raise
-    return aaa
-    # with httpx.Client() as client:
-    #     method = 'GET'
-    #     path = ''
-    #     url = os.environ["NEXTCLOUD_URL"] + path
-    #     userid = os.environ["NEXTCLOUD_USERID"]
-    #     secret = os.environ["NEXTCLOUD_SECRET"]
-    #     headers = {
-    #         'AUTHORIZATION-APP-API': base64.b64encode(bytes(f"{userid}:{secret}", 'utf-8')).decode('utf-8'),
-    #         'OCS-APIREQUEST': 'true',
-    #         'EX-APP-VERSION': 'v1.0.0',
-    #         'EX-APP-ID': os.environ['APP_ID'],
-    #         'AA-VERSION': 'v2.3.0'
-    #     }
-    #     print(f"sync_API_nextcloud_request: {url} - {method}", flush=True)
-    #     response = client.request(
-    #         method=method,
-    #         url=url,
-    #         params={'format': 'json'},
-    #         headers=headers,
-    #     )
-    #     print(
-    #         f"sync_API_nextcloud_request: method={method}, path={path}, status={response.status_code}",
-    #         flush=True
-    #     )
-    #     try:
-    #         response_data = json.loads(response.content)
-    #     except json.JSONDecodeError as e:
-    #         print(f"Error parsing JSON: {e}")
-    #         return []
-    #     print(json.dumps(response_data, indent=4), flush=True)
-    #     return response_data['ocs']['data']
+    r = nc.ocs("GET", "/ocs/v1.php/apps/webhook_listeners/api/v1/webhooks")
+    print("get_registered_listeners: ", json.dumps(r, indent=4), flush=True)
+    return r
 
 
-# async def delete_listener(registered_listener):
-#     async with httpx.AsyncClient() as client:
-#         method = 'DELETE'
-#         path = f"/ocs/v2.php/apps/webhook_listeners/api/v1/webhooks/{registered_listener['id']}"
-#         url = os.environ["NEXTCLOUD_URL"] + path
-#         userid = os.environ["NEXTCLOUD_USERID"]
-#         secret = os.environ["NEXTCLOUD_SECRET"]
-#
-#         headers = {
-#             'AUTHORIZATION-APP-API': base64.b64encode(bytes(f"{userid}:{secret}", 'utf-8')).decode('utf-8'),
-#             'OCS-APIREQUEST': 'true',
-#             'EX-APP-VERSION': 'v1.0.0',
-#             'EX-APP-ID': os.environ['APP_ID'],
-#             'AA-VERSION': 'v2.3.0'
-#         }
-#         print(f"sync_API_nextcloud_request: {url} - {method}", flush=True)
-#         response = await client.request(
-#             method=method,
-#             url=url,
-#             headers=headers,
-#             params={'format': 'json'}
-#         )
-#         print(
-#             f"sync_API_nextcloud_request: method={method}, path={path}, status={response.status_code}",
-#             flush=True
-#         )
-#         try:
-#             response_data = json.loads(response.content)
-#         except json.JSONDecodeError as e:
-#             print(f"Error parsing JSON: {e}")
-#             return
-#         print(json.dumps(response_data, indent=4), flush=True)
-#         return response_data['ocs']['data']
+async def delete_listener(registered_listener: dict):
+    r = NextcloudApp().webhooks.unregister(registered_listener["id"])
+    print("delete_listener: ", json.dumps(r, indent=4), flush=True)
+    return r
 
 
 if __name__ == "__main__":
