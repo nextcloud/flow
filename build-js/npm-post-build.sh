795263df437f90e810267a8183521ca22adbd4df
Oleksander Piskun <oleksandr2088@icloud.com>
2025-01-14 09:31:45 +0200

    fix: SPDX license script - preventing release in AppStore
    Signed-off-by: Oleksander Piskun <oleksandr2088@icloud.com>

--- build-js/npm-post-build.sh
+++ build-js/npm-post-build.sh
@@ -5,19 +5,30 @@
 
 set -e
 
-# Add licenses for source maps
-if [ -d "js" ]; then
-	for f in js/*.js; do
-		# If license file and source map exists copy license for the source map
-		if [ -f "$f.license" ] && [ -f "$f.map" ]; then
-			# Remove existing link
-			[ -e "$f.map.license" ] || [ -L "$f.map.license" ] && rm "$f.map.license"
-			# Create a new link
-			ln -s "$(basename "$f.license")" "$f.map.license"
-		fi
-	done
-	echo "Copying licenses for sourcemaps done"
-else
+# Directories to process (space-separated)
+directories="js ex_app/js"
+
+found_any_directory=false
+
+for dir in $directories; do
+	if [ -d "$dir" ]; then
+		found_any_directory=true
+		# Process all .js files in this directory
+		for f in "$dir"/*.js; do
+			# If license file and source map exists copy license for the source map
+			if [ -f "$f.license" ] && [ -f "$f.map" ]; then
+				# Remove existing link
+				[ -e "$f.map.license" ] || [ -L "$f.map.license" ] && rm "$f.map.license"
+				# Create a new link
+				ln -s "$(basename "$f.license")" "$f.map.license"
+			fi
+		done
+	fi
+done
+
+if [ "$found_any_directory" = false ]; then
 	echo "This script needs to be executed from the root of the repository"
 	exit 1
 fi
+
+echo "Copying licenses for sourcemaps done"
