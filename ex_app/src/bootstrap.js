8cbf149dffe5a3f09b3fc0d47883091936e8e917
Oleksander Piskun <oleksandr2088@icloud.com>
2025-06-05 11:17:21 +0300

    chore: use getCSPNonce instead of getRequestToken
    Signed-off-by: Oleksander Piskun <oleksandr2088@icloud.com>

--- ex_app/src/bootstrap.js
+++ ex_app/src/bootstrap.js
@@ -7,7 +7,7 @@ import Vue from 'vue'
 import { translate, translatePlural } from '@nextcloud/l10n'
 import { generateUrl } from '@nextcloud/router'
 import { APP_API_PROXY_URL_PREFIX, EX_APP_ID } from './constants/AppAPI.js'
-import { getRequestToken } from '@nextcloud/auth'
+import { getCSPNonce } from '@nextcloud/auth'
 
 Vue.prototype.t = translate
 Vue.prototype.n = translatePlural
@@ -15,4 +15,4 @@ Vue.prototype.OC = window.OC
 Vue.prototype.OCA = window.OCA
 
 __webpack_public_path__ = generateUrl(`${APP_API_PROXY_URL_PREFIX}/${EX_APP_ID}/js/`) // eslint-disable-line
-__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_nonce__ = getCSPNonce() // eslint-disable-line
